b8be5ace144f60da980cbe39f5dd0abeacdfac99
Akira Higuchi <higuchi@atama.plus>
2022-06-29 12:32:52 +0900

    fix enum usage.

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,11 +1,22 @@
-import { Fragment } from "react";
+import React from "react";
 import type { AppProps } from "next/app";
 import { Role } from "@model";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({}: AppProps) {
+  function canEdit(role: Role) {
+    return role === Role.ADMIN;
+  }
+
   return (
-    <Fragment>
-      <div>{Role.USER}</div>
-    </Fragment>
+    <>
+      <h1>Roles</h1>
+      <ul>
+        {Object.values(Role).map((role) => (
+          <li key={role}>
+            {role} / editable:{canEdit(role)}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
